831a9be784f881e368dbaeec687224db54f23d25
Sunny <me@darkowlzz.space>
2019-07-15 19:07:07 +0530

    Update rancher release for new dir structure
    Rancher charts dir structure changed to have separate directory for each
    releases. This change modifies the release script to create the
    appropriate dir structure and also sign-off the commits.
    
    The requirements include, creating a base for the new release using the
    existing latest release and committing it separately, and then adding
    the changes for new release on top of the copied base as another commit.

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,10 @@
 set -e
 
 # This script is used to publish storageos-operator chart to the upstream
-# rancher charts repo. It creates a diff of 
+# rancher charts repo. It creates a new base copy for the new version using
+# the latest release and copies the new changes to the new base copy. It then
+# creates a pull request with all the changes to the upstream rancher charts
+# repo.
 
 # Chart version.
 VERSION=$(yq r stable/storageos-operator/Chart.yaml version)
@@ -11,12 +14,14 @@ docker run --rm -ti \
     -v $PWD:/go/src/github.com/storageos/charts \
     -e GITHUB_USER=$GH_USER \
     -e GITHUB_EMAIL=$GH_EMAIL \
+    -e SIGN_OFF_NAME=$SIGN_OFF_NAME \
     -e GITHUB_TOKEN=$API_TOKEN \
     -e VERSION=$VERSION \
     -e TARGET_REPO="https://github.com/rancher/charts/" \
     -e FORK_REPO=$FORK_REPO \
     -e UPSTREAM_REPO_PATH="/go/src/github.com/rancher/charts" \
-    -e UPSTREAM_CHART_PATH="proposed/" \
-    -e CHART_ROOT=stable/storageos-operator/ \
+    -e UPSTREAM_CHART_PATH="proposed" \
+    -e CHART_NAME=$CHART_NAME \
+    -e CHART_ROOT=stable/storageos-operator \
     -w /go/src/github.com/storageos/charts \
     tianon/github-hub:2 bash -c "./scripts/create-pr.sh"
